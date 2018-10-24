3fdebf090e6b9f4f7d114ce7dad299ff4463c67d
Yunshuang <yuanyunshuang@163.com>
2018-10-24 21:09:20 +0200

    read all ply and translate them in 2d images, each image has gray values 0,50,100...

---
+++ GridMapRegistration/build/CMakeFiles/gridMapRegistration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gridMapRegistration.dir/src/main.cpp.o"
+  "gridMapRegistration.pdb"
+  "gridMapRegistration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gridMapRegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- GridMapRegistration/src/main.cpp
+++ GridMapRegistration/src/main.cpp
@@ -35,37 +35,9 @@ vector<string> getFiles(string cate_dir)
 	return files;
 }
 
-cv::Mat getGridMap(XYZ_p cloud, double resolution) {
-	
-	cv::Mat img(1000, 1000, CV_8UC1, cv::Scalar(0));
-	double min_z = numeric_limits<double>::max();
-	pcl::PointXYZ center(0,0,0);
-	for (int i = 0; i < cloud->points.size(); i++) {
-		center.x += cloud->points[i].x;
-		center.y += cloud->points[i].y;
-		center.z += cloud->points[i].z;
-//		if (cloud->points[i].z < min_z) {
-//			min_z = cloud->points[i].z;
-//		}
-	}
-	center.x /= cloud->points.size();
-	center.y /= cloud->points.size();
-	center.z/=cloud->points.size();
-	cout << min_z << endl;
-	for (int i = 0; i < cloud->points.size(); i++) {
-		pcl::PointXYZ p = cloud->points[i];
-		cv::Point2d ip((p.x - center.x) / resolution + img.cols / 2,
-				img.rows / 2 - (p.y - center.y) / resolution);
-		if (p.z > center.z&&p.z<center.z+1 && ip.x < img.cols && ip.y < img.rows && ip.x > 0
-				&& ip.y > 0) {
-			img.at<char>(ip.x, ip.y) = 255;
-		}
-	}
 
-	return img;
-}
 
-XYZ_p filterPointCloud(XYZ_p){
+XYZ_p filterPointCloud(XYZ_p cloud){
 	XYZ_p cloud_filtered(new XYZ);
 	pcl::VoxelGrid<pcl::PointXYZ> sor;
 	sor.setInputCloud(cloud);
@@ -75,8 +47,7 @@ XYZ_p filterPointCloud(XYZ_p){
 	return cloud_filtered;
 }
 
-void showGridMap(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, double resolution,
-		string name, cv::Mat img) {
+cv::Mat densityMap(XYZ_p cloud, double resolution, cv::Mat img) {
 
 	double min_z = numeric_limits<double>::max();
 	pcl::PointXYZ center(0,0,0);
@@ -92,41 +63,59 @@ void showGridMap(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, double resolution,
 	center.y /= cloud->points.size();
 	center.z/=cloud->points.size();
 	cout << min_z << endl;
+	cout << cloud->points.size() << endl;
 	for (int i = 0; i < cloud->points.size(); i++) {
 		pcl::PointXYZ p = cloud->points[i];
 		cv::Point2d ip((p.x - center.x) / resolution + img.cols / 2,
 				img.rows / 2 - (p.y - center.y) / resolution);
 		if (p.z > center.z&&p.z<center.z+1 && ip.x < img.cols && ip.y < img.rows && ip.x > 0
 				&& ip.y > 0) {
-			img.at<char>(ip.x, ip.y) = 255;
+			//cout<<int(ip.x)<<","<<int(ip.y)<<endl;
+			img.at<char>(ip.x, ip.y) += 50;
+			if(img.at<unsigned char>(ip.x, ip.y)>255)
+				img.at<unsigned char>(ip.x, ip.y) = 255;
 		}
 	}
-	cv::imshow(name, img);
+	/*for(int i=0; i<img.rows; i+=10){
+		for(int j=0; j<img.cols; j+=10)
+			cout<<int(img.at<char>(i, j))<<",";
+		cout<<endl;
+	}*/
+	//cout<<img.rows<<","<<img.cols<<endl;
+	//cv::namedWindow( "Display window",cv:: WINDOW_AUTOSIZE );
+	cv::imshow("image",img);
+
+	return img;
 }
 
 int main(int argc, char ** argv) {
-	if (argc < 3) {
-		std::cout << "Need two path to two pointclouds" << std::endl;
+	if (argc <3) {
+		std::cout << "Need one path to pointcloud, and one path for saving images" << std::endl;
 	}
+	string pt_path = argv[1];
+	string im_path = argv[2];
+	//get the file paths of the point clouds
+	vector<string> files = getFiles(pt_path);
 
-	XYZ_p cloud1(new XYZ);
-	XYZ_p cloud2(new XYZ);
+	//implemeting for each file: translate point cloud into 2D image
+	XYZ_p cloud(new XYZ);
 	pcl::PLYReader Reader;
-	Reader.read(argv[1], *cloud1);
-	Reader.read(argv[2], *cloud2);
-	cout << cloud1->points.size() << endl;
-	cout << cloud2->points.size() << endl;
-	cloud_filtered = filterPointCloud(cloud2);
-	double resolution = 0.1; //one pixel =0.1m
-	cv::Mat img1(1000, 1000, CV_8UC1, cv::Scalar(0));
-	cv::Mat img2(1000, 1000, CV_8UC1, cv::Scalar(0));
-	showGridMap(cloud1, resolution, "cloud1", img1);
-	showGridMap(cloud_filtered, resolution, "cloud2", img2);
-	cv::waitKey(0);
-	pcl::visualization::CloudViewer viewer("Cloud Viewer");
-	viewer.showCloud(cloud1);
-
-	while (!viewer.wasStopped()) {
+	string path;
+	for(int i=0; i<files.size(); i++){
+		cout<<pt_path +"/"+ files[i]<<endl;
+		Reader.read(pt_path +"/"+ files[i], *cloud);
+		cout << cloud->points.size() << endl;
+		//XYZ_p cloud_filtered = filterPointCloud(cloud);
+		double resolution = 0.1; //one pixel =0.1m
+		cv::Mat img(1000, 1000, CV_8UC1, cv::Scalar(0));
+		img = densityMap(cloud, resolution, img);
+		cv::waitKey(0);
+		path = files[i];
+		path = path.substr(0, path.length() - 4);
+		path = im_path + "/images/" + path + ".jpg";
+		cout<<"Saving image:"<<path<<endl;
+		cv::imwrite(path,img);
 	}
+
 	return 1;
 }
